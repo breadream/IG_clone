7ae2ee5cb02a48a1368f206bd08dd8f55b7cfa09
breadream <brad.dh.lim@gmail.com>
2019-01-03 18:43:05 -0600

    Added delete posts feature

--- instagram/app/controllers/posts_controller.rb
+++ instagram/app/controllers/posts_controller.rb
@@ -1,45 +1,57 @@
 class PostsController < ApplicationController
-	before_action :authenticate_user!
-	before_action :find_post, only: [:show]
+  before_action :authenticate_user!
+  before_action :find_post, only: [:show, :destroy]
 
-	def index
-		@posts = Post.all.limit(10).includes(:photos)
-		@post = Post.new
-	end
+  def index
+    @posts = Post.all.limit(10).includes(:photos, :user).order('created_at desc')
+    @post = Post.new
+  end
 
-	def create 
-		@post = current_user.posts.build(post_params)
-		if @post.save
-			if params[:images]
-				params[:images].each do |img|
-					@post.photos.create(image: params[:images][img])
-				end
-			end
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      if params[:images]
+        params[:images].each do |img|
+          @post.photos.create(image: params[:images][img])
+        end
+      end
 
-			redirect_to posts_path
-			flash[:notice] =  "Saved ..."
-		else
-			flash[:alert] = "Something went wrong..."
-			redirect_to posts_path
-		end
-	end
-	
-	def show
-	
-	end
+      redirect_to posts_path
+      flash[:notice] = "Saved ..."
+    else
+      flash[:alert] = "Something went wrong ..."
+      redirect_to posts_path
+    end
+  end
 
-	private
-	
-	def find_post
-		@post = Post.find_by id: params[:id]
-		
-		return if @post 
-		flash[:danger] = "Post doesn't exist!"
-		redirect_to root_path
-	end
+  def show
+    @photos = @post.photos
+  end
 
-	def post_params
-		params.require(:post).permit :content
-	end
+  def destroy
+    if @post.user == current_user
+      if @post.destroy
+        flash[:notice] = "Post deleted!"
+      else
+        flash[:alert] = "Something went wrong ..."
+      end
+    else
+      flash[:notice] = "You don't have permission to do that!"
+    end
+    redirect_to root_path
+  end
 
-end			
+  private
+
+  def find_post
+    @post = Post.find_by id: params[:id]
+
+    return if @post
+    flash[:danger] = "Post not exist!"
+    redirect_to root_path
+  end
+
+  def post_params
+    params.require(:post).permit :content
+  end
+end
